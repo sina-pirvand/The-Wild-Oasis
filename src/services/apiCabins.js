73c3257b4225f7ac7a980009d3c04e6f9e770b04
sina-prvnd <96621439+sina-prvnd@users.noreply.github.com>
2024-04-13 11:12:03 +0330

    create new cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,5 +1,6 @@
 import supabase from "./supabase";
 
+// READ CABINS
 export const getCabins = async () => {
   // supabase provided code
   const { data, error } = await supabase.from("cabins").select("*");
@@ -13,14 +14,31 @@ export const getCabins = async () => {
   return data;
 };
 
+// DELETE CABIN
 export const deleteCabin = async (id) => {
   // supabase doc provided code
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
   //
 
   if (error) {
-    console.log(error);
     throw new Error("Couldn't Delete Cabin");
+  }
+
+  return data;
+};
+
+// CREATE NEW CABIN
+export const createCabin = async (newCabin) => {
+  // SUPABASE API DOC CODE
+  const { data, error } = await supabase
+    .from("cabins")
+    // .insert([{ some_column: "someValue", other_column: "otherValue" }])
+    .insert([newCabin])
+    .select();
+  //
+
+  if (error) {
+    throw new Error("Couldn't Create Cabin");
   }
 
   return data;
